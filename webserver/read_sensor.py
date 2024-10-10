a6e843e8184a05bd2a7c3e89ffffecb985d285ff
Zhiqi Lu <zhiqilu8470@gmail.com>
2024-10-10 14:11:25 +0200

    refactor: integrated new code to read_sensor.py

--- webserver/read_sensor.py
+++ webserver/read_sensor.py
@@ -1,10 +1,13 @@
+import json
+import os
+
 from flask import Flask, jsonify, render_template_string, render_template, request
-# from sense_hat import SenseHat
+from sense_hat import SenseHat
 import time
 from threading import Thread
 
 app = Flask(__name__)
-# sense = SenseHat()
+sense = SenseHat()
 
 # Store data for the last 12 hours (5-minute intervals = 144 data points)
 data_history = []
@@ -12,29 +15,56 @@ desired_min_temp = -1.0
 desired_max_temp = -1.0
 desired_min_humi = -1.0
 desired_max_humi = -1.0
+json_file_path = 'sensor_data.json'
 
 def get_sensor_data():
     return {
-        'temperature': 10.11,
-        'humidity': 20.22,
-        'intensity': 1069,  # Assuming pressure as intensity
+        'temperature': round(sense.get_temperature(), 2),
+        'humidity': round(sense.get_humidity(), 2),
+        'intensity': round(sense.get_pressure(), 2),  # Assuming pressure as intensity
         'timestamp': time.time()
     }
-    # return {
-    #     'temperature': round(sense.get_temperature(), 2),
-    #     'humidity': round(sense.get_humidity(), 2),
-    #     'intensity': round(sense.get_pressure(), 2),  # Assuming pressure as intensity
-    #     'timestamp': time.time()
-    # }
+
+def save_to_json(data):
+    # Save data to JSON file (append mode)
+    with open(json_file_path, 'a+') as f:
+        f.seek(0)
+        try:
+            current_data = json.load(f)
+        except json.JSONDecodeError:
+            current_data = []
+        current_data.append(data)
+        f.seek(0)
+        f.truncate()
+        json.dump(current_data, f, indent=4)
+
+def read_last_12_hours():
+    if os.path.exists(json_file_path):
+        with open(json_file_path, 'r') as f:
+            try:
+                current_data = json.load(f)
+            except json.JSONDecodeError:
+                return []
+
+        # Filter data for the last 12 hours (43200 seconds)
+        cutoff_time = time.time() - 43200
+        last_12_hours_data = [entry for entry in current_data if entry['timestamp'] > cutoff_time]
+        return last_12_hours_data
+    else:
+        return []
 
 # Function to record data every 5 minutes
 def record_sensor_data():
+    global data_history
+    data_history = read_last_12_hours()  # Load the last 12 hours of data
+
     while True:
         data = get_sensor_data()
         # Maintain only 144 data points (5-minute intervals for 12 hours)
         if len(data_history) >= 720:
             data_history.pop(0)  # Remove the oldest data point
         data_history.append(data)
+        save_to_json(data)
         time.sleep(60)  # Sleep for 5 minutes
 
 @app.route('/sensor')
